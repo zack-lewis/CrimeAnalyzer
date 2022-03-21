2cd4d257ff1f76231eaf8eeec29e4e357b281ab4
zack-lewis <97800571+zack-lewis@users.noreply.github.com>
2022-03-20 21:33:22 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CrimeAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
